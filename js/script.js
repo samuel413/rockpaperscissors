41cb6dc37a5a0468fa6cbd7f29283454d9b438f4
samuel413 <sapereira@s.sfusd.edu>
2019-10-22 01:26:11 +0000

    added computer variables and need to make numbers interact and need to make human variable into numbers

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
-
+var computerOptions=['rock','paper','scissors','natural twenty']
+$('#shoot').click(function() {
+    var human=$('#input').val().toLowerCase();
+    var twoRN=Math.random();
+    if (twoRN < 0.33) {
+        var computer='0';
+    } else if (twoRN < 0.67) {
+        var computer='1';
+    } else {
+        var computer='2';
+    }
+});
 // DOCUMENT READY FUNCTION BELOW
 
